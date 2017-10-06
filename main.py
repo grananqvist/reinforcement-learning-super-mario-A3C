008667b2e548098cca34ab055bab9a77e7c3fbaf
Fredrik Kjellberg <fkakg@hotmail.com>
2017-10-06 17:06:14 +0200

    Add code for running several envs in parallel

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from dummy_agent import Agent
+from threading import Thread
+
 def main():
-  """ the main function """
+    """ the main function """
+    LEVEL_NAME = 'SuperMarioBros-1-1-v0'
+    NUMBER_OF_AGENTS = 1
+    EPISODES = 5
 
-  """ create an array of agents, size == number of threads available """
+    """ create an array of agents"""
+    agents = []
+    for i in range(0,NUMBER_OF_AGENTS):
+        agents.append(Agent(LEVEL_NAME))
 
+    """ run all agents in separate threads """
+    agent_threads = []
 
-  """ run all agents in separate threads """
-
-
-
+    for agent in agents:
+        f = lambda: agent.train(EPISODES)
+        t = Thread(target=f)
+        t.start()
+        agent_threads.append(t)
 
 
 
 if __name__ == '__main__':
-  main()
+    main()
