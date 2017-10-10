802ed2a9961208526c42b472fb12b7d6c93ec556
Filip Granqvist <fille_granqvist@hotmail.com>
2017-10-11 01:05:54 +0200

    fixed main to spawn new agent class

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from dummy_agent import Agent
+from agent import Agent
 from threading import Thread
+from A3C_network import A3CNetwork
+import tensorflow as tf
 
 def main():
     """ the main function """
-    LEVEL_NAME = 'SuperMarioBros-1-1-v0'
-    NUMBER_OF_AGENTS = 1
+    LEVEL_NAME = 'meta-SuperMarioBros-v0'
+    NUMBER_OF_AGENTS = 4
     EPISODES = 5
+
+    episode_count = tf.Variable(
+        0, 
+        dtype=tf.int32,
+        name='episode_count',
+        trainable=False
+    )
+
+    globalz = A3CNetwork((224,256,3), 6, 'global')
 
     """ create an array of agents"""
     agents = []
     for i in range(0,NUMBER_OF_AGENTS):
-        agents.append(Agent(LEVEL_NAME))
+        agents.append(Agent(LEVEL_NAME, 'agent_' + str(i), episode_count))
 
     """ run all agents in separate threads """
     agent_threads = []
 
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
     for agent in agents:
-        f = lambda: agent.train(EPISODES)
+        f = lambda: agent.train(sess, coord)
         t = Thread(target=f)
         t.start()
         agent_threads.append(t)
